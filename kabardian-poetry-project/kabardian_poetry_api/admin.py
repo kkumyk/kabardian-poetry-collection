4c206f8d039888a5636db93b4179ab58918a830f
kkumyk <k.kumykova@gmail.com>
2024-05-13 15:58:08 +0100

    adjust models by reducing them to poem and word models

--- kabardian-poetry-project/kabardian_poetry_api/admin.py
+++ kabardian-poetry-project/kabardian_poetry_api/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Poem, Vocabulary, Word
+from .models import Poem, Word
 
 admin.site.register(Poem)
-admin.site.register(Vocabulary)
 admin.site.register(Word)
--- kabardian-poetry-project/kabardian_poetry_api/models.py
+++ kabardian-poetry-project/kabardian_poetry_api/models.py
@@ -5,17 +5,26 @@ from django.contrib.postgres.fields import JSONField
 
 class Word(models.Model):
         word = models.CharField(max_length=100)
+        type = models.CharField(max_length=100, default='noun')
         eng_transl = models.CharField(max_length=100)
-        kab_transl = models.CharField(max_length=100)
+        rus_transl = models.CharField(max_length=100)
         ipa = models.CharField(max_length=100)
         # audio_url = models.URLField(max_length=100)
         
-class Vocabulary(models.Model):
-    words = models.ManyToManyField(Word, related_name='vocabulary_words')
+        def __str__(self):
+            return self.eng_transl
+                    
+                    
+# class Vocabulary(models.Model):
+#     type = models.CharField(max_length=100, default='nouns')
+#     words = models.ManyToManyField(Word, related_name='vocabulary_words')
 
 class Poem(models.Model):
     name = models.CharField(max_length=300)
     author = models.CharField(max_length=200)
     # audio_url = models.URLField(max_length=300)
     text = models.TextField()
-    vocabulary = models.ForeignKey(Vocabulary, on_delete=models.CASCADE)
+    vocabulary = models.ForeignKey(Word, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
