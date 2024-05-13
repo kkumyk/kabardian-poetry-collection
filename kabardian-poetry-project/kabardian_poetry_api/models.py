86943d8a69896845978c2451768d94f928821d09
kkumyk <k.kumykova@gmail.com>
2024-05-13 15:02:10 +0100

    remove audio url fields from the word and poem models (for now)

--- kabardian-poetry-project/kabardian_poetry_api/models.py
+++ kabardian-poetry-project/kabardian_poetry_api/models.py
@@ -8,7 +8,7 @@ class Word(models.Model):
         eng_transl = models.CharField(max_length=100)
         kab_transl = models.CharField(max_length=100)
         ipa = models.CharField(max_length=100)
-        audio_url = models.URLField(max_length=100)
+        # audio_url = models.URLField(max_length=100)
         
 class Vocabulary(models.Model):
     words = models.ManyToManyField(Word, related_name='vocabulary_words')
@@ -16,6 +16,6 @@ class Vocabulary(models.Model):
 class Poem(models.Model):
     name = models.CharField(max_length=300)
     author = models.CharField(max_length=200)
-    audio_url = models.URLField(max_length=300)
+    # audio_url = models.URLField(max_length=300)
     text = models.TextField()
     vocabulary = models.ForeignKey(Vocabulary, on_delete=models.CASCADE)
