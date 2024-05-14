191afe00973f42a89fa4af207d575e4d895f2923
kkumyk <k.kumykova@gmail.com>
2024-05-14 15:05:29 +0100

    connect to postgres

--- kabardian-poetry-project/kabardian_poetry_api/settings.py
+++ kabardian-poetry-project/kabardian_poetry_api/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,11 +80,20 @@ WSGI_APPLICATION = 'kabardian_poetry_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+
+# we need to comment out the local sqlite3 database connection.
+# Django provides this local sqlite3 database by default for development:
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# connect to the environment variable DATABASE_URL you previously added to the env.py file:
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
