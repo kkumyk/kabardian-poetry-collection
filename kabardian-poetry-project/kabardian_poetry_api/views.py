e92783d4dc1adf4e7853788ee9d6e3ae4b3e5ef1
kkumyk <k.kumykova@gmail.com>
2024-05-15 14:27:23 +0100

    update GET request of poem_detail view

--- kabardian-poetry-project/kabardian_poetry_api/views.py
+++ kabardian-poetry-project/kabardian_poetry_api/views.py
@@ -6,14 +6,13 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-# get a GET request to return all existing poems in the db:
 
 @api_view(['GET', 'POST'])
 def poem_list(request):
     if request.method == 'GET':
         poems = Poem.objects.all()
         serializer =  PoemSerializer(poems, many=True)
-        return JsonResponse({'poems': serializer.data})
+        return Response({'poems': serializer.data})
 
     if request.method == 'POST':
         serializer = PoemSerializer(data=request.data)
@@ -21,27 +20,31 @@ def poem_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-
-
-# get a GET request to return a poem by id and include poems' words:
+@api_view(['GET','PUT', 'DELETE'])
 def poem_detail(request, id):
-    # get the poem instance or return a 404 response if not found
-    poem = get_object_or_404(Poem, pk=id)
     
-    # serialize the poem instance
-    poem_serializer = PoemSerializer(poem)
+    # check if it is a valid request
+    try:
+        poem = Poem.objects.get(pk=id)
+    except Poem.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND) # get the poem instance or return a 404 response if not found
     
-    # Get poem's words
-    words = Word.objects.filter(poem=poem)
-    
-    # serialize poem's words
-    word_serializer = WordSerializer(words, many=True)
-    
-    # combine poem data and words data into a single dictionary
-    data = {
-        'poem': poem_serializer.data,
-        'words': word_serializer.data
-    }
-    
-    # return the combined data as JSON response
-    return JsonResponse(data)
+    if request.method == 'GET':
+        poem_serializer = PoemSerializer(poem) # serialize the poem instance
+        
+        words = Word.objects.filter(poem=poem) # get poem's words
+        word_serializer = WordSerializer(words, many=True) # serialize poem's words
+        
+        # combine poem data and words data into a single dictionary
+        data = {
+            'poem': poem_serializer.data,
+            'words': word_serializer.data
+        }
+        
+        # return the combined data as JSON response
+        return Response(data)
+
+    elif request.method == 'PUT':
+        pass
+    elif request.method == 'DELETE':
+        pass
