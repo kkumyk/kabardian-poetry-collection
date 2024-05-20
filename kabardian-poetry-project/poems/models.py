70f9177c0d11d226a556f3f66cb68bbbebc87de7
kkumyk <k.kumykova@gmail.com>
2024-05-20 13:56:55 +0100

    remove unused import

--- kabardian-poetry-project/poems/models.py
+++ kabardian-poetry-project/poems/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
 class Word(models.Model):
         word = models.CharField(max_length=100, unique=True)
