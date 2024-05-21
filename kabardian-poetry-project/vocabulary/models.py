49879b3bcce24bddbdd3233aa37035e6f5432bf5
kkumyk <k.kumykova@gmail.com>
2024-05-21 14:50:01 +0100

    extend vocabulary model with the user's name

--- kabardian-poetry-project/vocabulary/models.py
+++ kabardian-poetry-project/vocabulary/models.py
@@ -11,4 +11,29 @@ class Vocabulary(models.Model):
         unique_together = ('user', 'word') # the same word cannot be added multiple times to the same user's vocabulary;
 
     def __str__(self):
-        return f"{self.word.word}" # {self.user.username} - 
+        return f"{self.user.username} - {self.word.word}" 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
