664444a4c2bc199682248389fcf1f668fbaf522f
kkumyk <k.kumykova@gmail.com>
2024-05-24 14:35:14 +0100

    add DeleteWordForm class

--- kabardian-poetry-project/vocabulary/forms.py
+++ kabardian-poetry-project/vocabulary/forms.py
@@ -2,3 +2,7 @@ from django import forms
 
 class AddWordForm(forms.Form):
     word = forms.CharField(label='Word', max_length=250)
+
+
+class DeleteWordForm(forms.Form):
+    vocabulary_entry = forms.CharField(label='Word', max_length=250)
