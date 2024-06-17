117a6c72ae45d4af37995ce77412458056d61789
kkumyk <k.kumykova@gmail.com>
2024-06-17 19:36:15 +0100

    remove "type" field from the Word model

--- poems/models.py
+++ poems/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Word(models.Model):
         word = models.CharField(max_length=100, unique=True)
-        type = models.CharField(max_length=100)
         eng_transl = models.CharField(max_length=100)
         rus_transl = models.CharField(max_length=100)
         ipa = models.CharField(max_length=100)
