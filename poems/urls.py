82dacc8441f1bc26f680fe4e405c343c3616bd31
kkumyk <k.kumykova@gmail.com>
2024-07-24 22:34:09 +0100

    update url patterns for poems app

--- poems/urls.py
+++ poems/urls.py
@@ -1,21 +1,13 @@
 # This file is where we'll list our poems app-specific URLs.
 
 from django.urls import path
-# from . import views
-from .views import PoemList, poem_detail_ui, poems_list_api_view, poem_detail
+from .views import PoemList, poem_detail_ui, poems_list_api_view, poem_detail_api_view
 
 
 urlpatterns = [
-    
-    # UI URL paths:
     path('', PoemList.as_view(), name='poems_list'), # TODO
     path('poems/', PoemList.as_view(), name='poems_list'), # TODO
     path('poems/<int:id>/', poem_detail_ui, name='poem_detail_ui'),
-    
-    # API URL paths:
-    # path('poems-api/', poem_list, name='poems'),
-    path('poems-api/', poems_list_api_view, name='poems_list_api_view'),
-    
-    path('poems-api/<int:id>/', poem_detail),
-    
+    path('poems/api/', poems_list_api_view, name='poems_list_api_view'),
+    path('poems/api/<int:id>/', poem_detail_api_view, name='poems_detail_api_view')
 ]
