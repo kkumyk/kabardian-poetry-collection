5c208f5f43ed76a5d07b26de26a9b506b1bed02f
kkumyk <k.kumykova@gmail.com>
2024-08-27 12:08:30 +0100

    adjust word and poem models to name poem_id and word_id explicitly

--- poems/models.py
+++ poems/models.py
@@ -1,25 +1,23 @@
 from django.db import models
 
 class Word(models.Model):
-        word = models.CharField(max_length=200, unique=True)
-        eng_transl = models.CharField(max_length=1000)
-        rus_transl = models.CharField(max_length=1000)
+        id = models.AutoField(primary_key=True, db_column='word_id')
+        word = models.TextField()
+        eng_transl = models.TextField()
+        rus_transl = models.TextField()
         
         def __str__(self):
             return self.eng_transl
 
 
 class Poem(models.Model):
-    title = models.CharField(max_length=300, unique=True)
-    author = models.CharField(max_length=200)
+    id = models.AutoField(primary_key=True, db_column='poem_id')
+    title = models.TextField()
+    author = models.TextField()
     contents = models.TextField()
     words = models.ManyToManyField(Word)
     
     def __str__(self):
         return self.title
-    
-# The __str__() dunder method lets you represent your class object as a string for the benefit of your app's user.
 
-# SELECT * FROM poems_poem p
-# JOIN poems_poem_words pw on pw.poem_id = p.id
-# JOIN poems_word w on w.id = pw.word_id;
+# The __str__() dunder method lets you represent your class object as a string for the benefit of your app's user.
